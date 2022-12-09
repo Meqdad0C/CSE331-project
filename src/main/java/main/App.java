40460a55b81a66aabdc55b9e541595a387d2585e
Meqdad Amr <69355465+Meqdad0C@users.noreply.github.com>
2022-12-09 09:38:19 +0200

    Test toJson

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -6,6 +6,9 @@ public class App {
     public static void main(String[] args) {
         Reader reader=new Reader("input.xml");
         Users users=new Users(reader.getTagData(),reader.getTagsQueue());
-        System.out.println(users.users.get(0).getName());
+        //System.out.println(users.users.get(0));
+        System.out.println();
+        System.out.println();
+        System.out.println(users.users.get(0).toJson());
     }
 }
