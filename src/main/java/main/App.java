e17cc380b5f623e20e9087984d01d29cc4348bea
Meqdad Amr <meqdad.dodo.25@gmail.com>
2022-12-11 21:48:22 +0200

    Test getMutualFollowers

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,11 +1,13 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
         Reader reader=new Reader("input.xml");
         Users users=new Users(reader.getTagData(),reader.getTagsQueue());
+        List<User> usersList=users.getUsers();
         //System.out.println(users.users.get(0));
         System.out.println();
         System.out.println();
@@ -14,5 +16,7 @@ public class App {
         System.out.println(users.prettyJSON());
         System.out.println(users.prettyXML2());
         System.out.println(users.getMostFollowedUser());
+
+        System.out.println(Users.getMutualFollowers(usersList.get(3),usersList.get(0)));
     }
 }
