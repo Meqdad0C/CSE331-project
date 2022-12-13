11a97ddfeb0ef344467b81177c15a2093b51ecf1
Meqdad Amr <69355465+Meqdad0C@users.noreply.github.com>
2022-12-13 08:01:15 +0200

    Add Skeleton for Consistency class

--- src/main/java/main/Consistency.java
+++ src/main/java/main/Consistency.java
@@ -1,30 +1,41 @@
 package main;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
 public class Consistency {
-    private List<String> tags;
-    List<Boolean> isOpenClose;
+    private Reader reader;
 
-    private Stack<String> tagsStack = new Stack<>();
 
-    public Consistency(List<String> tags,List<Boolean> isOpenClose ) {
-        this.tags = tags;
-        this.isOpenClose=isOpenClose;
+    public Consistency(Reader reader) {
+        this.reader = reader;
     }
-    public void correctTags(){
-        for(int i=0;i<tags.size();i++){
-            if(isOpenClose.get(i)==true){
-                tagsStack.push(tags.get(i));
-            }
-            else{
-                if(tagsStack.pop().equals(tags.get(i)));
-                else{
 
-                }
-            }
-        }
+    public void isConsistent() {
+        Stack<String> tagsStack = reader.getTagsStack();
+        List<String> tagsQueue = reader.getTagsQueue();
+        List<Boolean> isOpenClose = reader.getIsOpenClose();
+        List<String> tagNames = reader.getTagNames();
+        List<List<Integer>> tagsIndex = reader.getTagsIndex();
+        List<List<Integer>> openTagsIndices = reader.getOpenTagsIndices();
+        List<List<Integer>> closedTagsIndices = reader.getClosedTagsIndices();
+        List<Integer> lineNumbers = reader.getLineNumbers();
+        List<String> openTagsOnly = reader.getOpenTagsOnly();
+        List<String> closedTagsOnly = reader.getClosedTagsOnly();
+
+        // check the first open tag is the same as the last closed tag
+
+
+    }
+
+
+    // Main method
+    public static void main(String[] args) {
+        String fileName = "test.xml";
+        Reader reader = new Reader(fileName);
+        // Create a new instance of the class
+        Consistency validator = new Consistency(reader);
+        validator.isConsistent();
+
     }
 }
